3c08b0c9466151f05848ba68b93c450146ef8671
machnmb_ <mannybiz@outlook.com>
2024-02-26 09:49:55 -0600

    fixing redirection to website

--- urlshortenerapp/src/pages/[codes].tsx
+++ urlshortenerapp/src/pages/[codes].tsx
@@ -1,37 +1,40 @@
 import { getURL } from "@/utils/url";
 import { GetServerSideProps } from "next";
 
-export default function WebRedirect(){
-
-    return null;
-}
+// export default function WebRedirect(){
+//     return <div>
+//         Redirecting...
+//     </div>>
+// }
 
 export const getServerSideProps = async(context:any) => {
-    console.log(context)
+    console.log("Context:", context)
+
+    console.log("Context params:", context.params)
 
     const codes = context.params?.codes
     console.log(codes)
 
 
-    // if(!codes){
-    //     console.error('URL code not found')
-    //     return {notFound: true}
-    // }
+    if(!codes){
+        console.error('URL code not found')
+        return {notFound: true}
+    }
 
-    // const originalUrl = await getURL(codes as string);
-    // console.log(originalUrl)
+    const originalUrl = await getURL(codes);
+    console.log(originalUrl)
 
-    // if(originalUrl) {
-    //     console.log(originalUrl)
-    //     return {
-    //         redirect: {
-    //             destination: originalUrl.url,
-    //             permanent:false
-    //         },
-    //     };
-    // } else {
-    //     return {notFound: true}
-    // }
+    if(originalUrl) {
+        console.log(originalUrl)
+        return {
+            redirect: {
+                destination: originalUrl.url,
+                permanent:false
+            },
+        };
+    } else {
+        return {notFound: true}
+    }
 }
 
 
